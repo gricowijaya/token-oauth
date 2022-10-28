68974a369cb5ae4719b73debcc728a46b2564d04
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-29 02:16:55 +0800

    add routes for facebook endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const controllers = require('../controllers');
 
 // login endpoint /api/auth/login
 router.use('/auth/login', controllers.auth.google);
+router.use('/auth/login/facebook', controllers.auth.facebook);
 
 module.exports = router;
