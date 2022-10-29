a783c26735169dbb7add2ee0f4f789fb3d8dded0
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-29 12:18:51 +0800

    add email fields in user

--- db/migrations/20221028143655-create-user.js
+++ db/migrations/20221028143655-create-user.js
@@ -11,6 +11,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      email: {
+        type: Sequelize.STRING
+      },
       username: {
         type: Sequelize.STRING
       },
