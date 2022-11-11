a8334f05ee333f167d5fe85c1e2611732b4bed19
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-11-11 12:58:14 +0800

    add sentry and mailer

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ const controllers = require('../controllers');
 router.get('/auth/login/google', controllers.auth.google);
 router.get('/auth/login/facebook', controllers.auth.facebook);
 
+// debug sentry the integration
+router.get('/debug/sentry', controllers.sentry.mainHandler);
+
+// sending endpoint
+router.get('/mailer/send-email', controllers.mailer.sendEmail);
+
 module.exports = router;
